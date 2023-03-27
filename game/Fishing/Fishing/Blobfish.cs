ebd53e3350c7484ad60a1e8c0e741ce0256cbfe6
aac4114 <aac4114@rit.edu>
2023-03-27 12:50:28 -0400

    Added class stub for Blobfish and removed Inventory class since we aren't using it right now (can always add it back in later)

---
+++ game/Fishing/Fishing/Blobfish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Team Tranquil
+ * GDAPS 2 Project
+ * Class purpose: Parent class for items that can be picked up by the player, specifically Fish and Books
+ * 
+ * Known issues: incomplete
+ * 
+ */
+namespace Fishing
+{
+    internal class Blobfish
+    {
+        //The blobfish
+
+        //TODO: Add in texture and textbox functionality
+    }
+}
